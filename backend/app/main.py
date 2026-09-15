9f0f2139c9979eed98eb035cc1c89876dc13a13a
22130030 <22130030@st.hcmuaf.edu.vn>
2026-09-15 11:09:55 +0700

    Use Render API in frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173"
+        "https://taskflow-api-3b5f.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
