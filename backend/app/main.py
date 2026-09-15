9e3bbfb69b391286b04e6f2a9ca87c0d737aff42
22130030 <22130030@st.hcmuaf.edu.vn>
2026-09-15 11:23:29 +0700

    Fix CORS for Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,31 +7,24 @@ from app.routers import (
     tasks
 )
 
-
 app = FastAPI(
     title="Task Management API",
     version="1.0.0"
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://task-management-flame-ten.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-        "https://task-management-flame-ten.vercel.app"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(tasks.router)
-
-
-@app.get("/")
-def home():
-    return {
-        "message": "Task Management API is running"
-    }
